f8f5b64438ddeb936de39edaab5ee8574521d398
bestman8 <35968731+bestman8@users.noreply.github.com>
2021-03-14 14:09:43 +0100

    Delete install.py

--- luxcore/install.py
+++
@@ -1,4 +0,0 @@
-import bpy
-bpy.ops.preferences.addon_install(filepath='/gdrive/MyDrive/luxcore/BlendLuxCore-latest-linux64.zip')
-bpy.ops.preferences.addon_enable(module='BlendLuxCore')
-bpy.ops.wm.save_userpref()
